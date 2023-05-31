c05b958cc419d51810b79ace4d18be18225e737d
Steffen Slavetinsky <steffen.slavetinsky@renumics.com>
2023-05-31 15:31:11 +0200

    :lipstick: Add copy icon to commit

---
+++ frontend/src/icons/Copy.tsx
@@ -0,0 +1,9 @@
+import type { IconType } from 'react-icons';
+import { RiFileCopyLine } from 'react-icons/ri';
+import tw from 'twin.macro';
+
+const Docs: IconType = tw(
+    RiFileCopyLine
+)`w-4 h-4 font-bold inline-block align-middle stroke-current`;
+
+export default Docs;
