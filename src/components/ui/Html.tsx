d6173c8c8d4dd701c10ab4ec43fffeb59ecaafb6
Dominik Haentsch <dominik.haentsch@gmail.com>
2023-06-01 16:08:41 +0200

    style raw html with tailwind typography

--- src/components/ui/Html.tsx
+++ src/components/ui/Html.tsx
@@ -1,7 +1,7 @@
 import tw, { styled } from 'twin.macro';
 
 const StyledHtml = styled.div`
-    ${tw`text-sm content-center items-center h-full w-full m-1 overflow-y-scroll`}
+    ${tw`text-sm content-center items-center h-full w-full m-1 overflow-y-scroll prose`}
 `;
 
 interface Props {
