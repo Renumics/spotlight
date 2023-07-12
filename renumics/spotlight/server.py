763f8135158f0b2e1b9809ce74915dc368d536b3
Dominik Haentsch <dominik.haentsch@gmail.com>
2023-07-12 13:25:15 +0200

    redirect output of server process to /dev/null when verbose=False

--- renumics/spotlight/server.py
+++ renumics/spotlight/server.py
@@ -156,6 +156,8 @@ class Server:
             command,
             env=env,
             pass_fds=None if platform.system() == "Windows" else (sock.fileno(),),
+            stdout=subprocess.STDOUT if settings.verbose else subprocess.DEVNULL,
+            stderr=subprocess.STDOUT if settings.verbose else subprocess.DEVNULL,
         )
         if platform.system() != "Windows":
             sock.close()
