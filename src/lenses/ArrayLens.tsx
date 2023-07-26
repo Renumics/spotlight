e24cdc0428c1fe40cdf5867606f962001e036387
Alexander Druz <alexandersdruz@gmail.com>
2023-07-26 15:44:15 +0200

    Format nested arrays in array lens

--- src/lenses/ArrayLens.tsx
+++ src/lenses/ArrayLens.tsx
@@ -2,18 +2,34 @@ import 'twin.macro';
 import dataformat from '../dataformat';
 import { Lens } from '../types';
 
+interface ArrayProps {
+    value: Array<number>;
+}
+
+const ArrayComponent = ({ value }: ArrayProps): JSX.Element => {
+    return (
+        <>
+            <div tw="font-bold">[</div>
+            {value.map((element, i) =>
+                Array.isArray(element) ? (
+                    <ArrayComponent value={element} />
+                ) : (
+                    <div tw="m-1" key={i}>
+                        {dataformat.formatNumber(element)}
+                    </div>
+                )
+            )}
+            <div tw="font-bold">]</div>
+        </>
+    );
+};
+
 const ArrayLens: Lens = ({ value }) => {
     const array = value as Array<number>;
 
     return (
         <div tw="text-xs px-1 py-0.5 flex flex-wrap content-center items-center h-full">
-            <div tw="font-bold">[</div>
-            {array.map((value, i) => (
-                <div tw="m-1" key={i}>
-                    {dataformat.formatNumber(value)}
-                </div>
-            ))}
-            <div tw="font-bold">]</div>
+            <ArrayComponent value={array} />
         </div>
     );
 };
