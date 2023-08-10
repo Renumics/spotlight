78f9bbd51de3af64201c7e573895f50fa529734f
Steffen Slavetinsky <steffen.slavetinsky@renumics.com>
2023-08-10 16:05:24 +0200

    :lipstick: Add hf card

--- src/CTADialog.tsx
+++ src/CTADialog.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { ReactNode, useCallback, useEffect, useState } from 'react';
 import Dialog from './components/ui/Dialog';
 import tw from 'twin.macro';
 import Button from './components/ui/Button';
@@ -18,11 +18,37 @@ const FadeInDiv = styled.div`
     animation: ${fadeIn} 0.35s ease-in-out;
 `;
 
-const CTACard = styled.div`
+const StyledCTACard = styled.div`
     animation: ${fadeIn} 0.7s ease-in-out;
 `;
 
-const CTA_TIMEOUT_MS = 30 * 1000;
+const CTACard = ({
+    href,
+    background,
+    children,
+}: {
+    href: string;
+    background: string;
+    children: ReactNode;
+}) => (
+    <StyledCTACard
+        className="group"
+        tw="h-64 bg-white rounded shadow-md cursor-pointer relative overflow-hidden"
+    >
+        <div
+            tw="w-full h-full absolute group-hover:opacity-95"
+            style={{
+                background,
+            }}
+        ></div>
+        <div tw="relative w-full h-full">
+            <a tw="h-full w-full p-4 block" href={href} target="_spotlight_cta">
+                {children}
+            </a>
+        </div>
+    </StyledCTACard>
+);
+const CTA_TIMEOUT_MS = 1 * 1000;
 
 const collections = [
     {
@@ -41,14 +67,14 @@ const collections = [
         description:
             'Integrate your data and uncover fascinating insights tailored just for you.',
     },
-    {
-        name: 'Explore more Spaces',
-        href: '#',
-        imageSrc:
-            'https://tailwindui.com/img/ecommerce-images/home-page-04-collection-03.jpg',
-        description:
-            'Have a look at the various other examples we created ready to explore.',
-    },
+    // {
+    //     name: 'Explore more Spaces',
+    //     href: '#',
+    //     imageSrc:
+    //         'https://tailwindui.com/img/ecommerce-images/home-page-04-collection-03.jpg',
+    //     description:
+    //         'Have a look at the various other examples we created ready to explore.',
+    // },
 ];
 
 const CTADialog = () => {
@@ -86,22 +112,14 @@ const CTADialog = () => {
                 <div tw="absolute w-full mt-6">
                     <div tw="grid grid-cols-3 space-x-4 px-4">
                         {collections.map((collection) => (
-                            <CTACard
-                                key={collection.name}
-                                className="group"
-                                tw="h-64 bg-white rounded shadow-md cursor-pointer relative overflow-hidden"
-                            >
+                            <CTACard key={collection.name} href={collection.href}>
                                 <img
-                                    src={collection.imageSrc}
+                                    src={}
                                     tw="object-cover w-full h-full absolute group-hover:opacity-75"
                                     alt={collection.name}
                                 />
                                 <div tw="w-full h-full relative group-hover:opacity-90">
-                                    <a
-                                        tw="h-full w-full"
-                                        alt={collection.name}
-                                        href={collection.href}
-                                    >
+                                    <a tw="h-full w-full" alt={collection.name}>
                                         <h3 tw="px-4 pt-1 text-xl tracking-wide text-gray-900">
                                             {collection.name}
                                         </h3>
@@ -109,6 +127,19 @@ const CTADialog = () => {
                                 </div>
                             </CTACard>
                         ))}
+                        <CTACard
+                            href="https://renumics.com/docs/getting-started/?source=spotlight_cta#or-jump-right-into-using-spotlight-in-one-of-our-hosted-huggingface--spaces"
+                            background="linear-gradient(15deg, rgba(126,63,251,1) 0%, rgba(252,70,107,1) 100%)"
+                        >
+                            <span tw="text-5xl">🤗</span>
+                            <h3 tw="pt-2 text-xl tracking-wide font-extrabold text-white">
+                                Explore more Spaces
+                            </h3>
+                            <p tw="pt-2 text-base text-white">
+                                Have a look at other Spotlight use cases hosted on
+                                huggingface.
+                            </p>
+                        </CTACard>
                     </div>
                 </div>
             </FadeInDiv>
