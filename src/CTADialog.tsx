fa21e4320582559dff25d7c2d98817c0e1d2cbdc
Steffen Slavetinsky <steffen.slavetinsky@renumics.com>
2023-08-10 16:32:39 +0200

    :construction: Add content

--- src/CTADialog.tsx
+++ src/CTADialog.tsx
@@ -23,20 +23,22 @@ const StyledCTACard = styled.div`
 `;
 
 const CTACard = ({
+    className,
     href,
     background,
     children,
 }: {
+    className?: string;
     href: string;
     background: string;
     children: ReactNode;
 }) => (
     <StyledCTACard
-        className="group"
+        className={'group' + (className ? ' ' + className : '')}
         tw="h-64 bg-white rounded shadow-md cursor-pointer relative overflow-hidden"
     >
         <div
-            tw="w-full h-full absolute group-hover:opacity-95"
+            tw="w-full h-full absolute group-hover:opacity-90"
             style={{
                 background,
             }}
@@ -48,34 +50,7 @@ const CTACard = ({
         </div>
     </StyledCTACard>
 );
-const CTA_TIMEOUT_MS = 1 * 1000;
-
-const collections = [
-    {
-        name: 'Explore Locally',
-        href: '#',
-        imageSrc:
-            'https://tailwindui.com/img/ecommerce-images/home-page-04-collection-01.jpg',
-        description:
-            'Curious about how our Spotlight performs offline? Delve into its features on your own computer.',
-    },
-    {
-        name: 'Try it on your own Data',
-        href: '#',
-        imageSrc:
-            'https://tailwindui.com/img/ecommerce-images/home-page-04-collection-02.jpg',
-        description:
-            'Integrate your data and uncover fascinating insights tailored just for you.',
-    },
-    // {
-    //     name: 'Explore more Spaces',
-    //     href: '#',
-    //     imageSrc:
-    //         'https://tailwindui.com/img/ecommerce-images/home-page-04-collection-03.jpg',
-    //     description:
-    //         'Have a look at the various other examples we created ready to explore.',
-    // },
-];
+const CTA_TIMEOUT_MS = 30 * 1000;
 
 const CTADialog = () => {
     const [isVisible, setIsVisible] = useState(false);
@@ -102,43 +77,45 @@ const CTADialog = () => {
                 </Button>
                 <div tw="p-16 pb-2">
                     <h2 tw="text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                        Continue your Journey with Spotlight
+                        Thanks for giving Spotlight a spin!
                     </h2>
                     <p tw="mt-3 text-center text-xl text-gray-900">
-                        Dive deeper into the features and possibilities Spotlight has to
-                        offer.
+                        How can we assist you in digging deeper into Spotlight and
+                        speeding up your data-centric AI workflows?
                     </p>
                 </div>
                 <div tw="absolute w-full mt-6">
                     <div tw="grid grid-cols-3 space-x-4 px-4">
-                        {collections.map((collection) => (
-                            <CTACard key={collection.name} href={collection.href}>
-                                <img
-                                    src={}
-                                    tw="object-cover w-full h-full absolute group-hover:opacity-75"
-                                    alt={collection.name}
-                                />
-                                <div tw="w-full h-full relative group-hover:opacity-90">
-                                    <a tw="h-full w-full" alt={collection.name}>
-                                        <h3 tw="px-4 pt-1 text-xl tracking-wide text-gray-900">
-                                            {collection.name}
-                                        </h3>
-                                    </a>
-                                </div>
-                            </CTACard>
-                        ))}
+                        <CTACard background="" href="" tw="bg-gray-400">
+                            <h3 tw="pt-2 text-xl tracking-wide font-extrabold text-white">
+                                Read background article
+                            </h3>
+                            <p tw="pt-2 text-base text-white">
+                                Step-by-step instructions for the use case
+                            </p>
+                        </CTACard>
+                        <CTACard background="" href="" tw="bg-gray-400">
+                            <h3 tw="pt-2 text-xl tracking-wide font-extrabold text-white">
+                                Apply pre-defined template
+                            </h3>
+                            <p tw="pt-2 text-base text-white">
+                                Load your own data in less than{' '}
+                                <b tw="font-bold">10 minutes</b>.
+                            </p>
+                        </CTACard>
                         <CTACard
                             href="https://renumics.com/docs/getting-started/?source=spotlight_cta#or-jump-right-into-using-spotlight-in-one-of-our-hosted-huggingface--spaces"
                             background="linear-gradient(15deg, rgba(126,63,251,1) 0%, rgba(252,70,107,1) 100%)"
+                            tw="bg-gray-400"
                         >
-                            <span tw="text-5xl">🤗</span>
                             <h3 tw="pt-2 text-xl tracking-wide font-extrabold text-white">
-                                Explore more Spaces
+                                Don’t know yet
                             </h3>
                             <p tw="pt-2 text-base text-white">
-                                Have a look at other Spotlight use cases hosted on
-                                huggingface.
+                                Explore more interactive use cases hosted on{' '}
+                                <span tw="font-bold">huggingface</span>.
                             </p>
+                            <span tw="text-5xl float-right mt-8">🤗</span>
                         </CTACard>
                     </div>
                 </div>
