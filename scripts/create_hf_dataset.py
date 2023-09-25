e90ba76fc7f336ff551ea98e9d9053c23ee23925
Alexander Druz <alexandersdruz@gmail.com>
2023-09-25 11:51:21 +0200

    Test more HF dtypes

--- scripts/create_hf_dataset.py
+++ scripts/create_hf_dataset.py
@@ -6,6 +6,7 @@ This script creates multimodal Hugging Face dataset to test Spotlight on.
 
 import datasets
 import numpy as np
+from renumics import spotlight
 
 from renumics.spotlight import dtypes
 
@@ -44,20 +45,74 @@ def random_values(
 def create_hf_dataset(num_rows: int) -> None:
     ds = datasets.Dataset.from_dict(
         {
-            # "bool": random_values(dtypes.bool_dtype, num_rows),
+            "bool": [True, False, False],
+            "int": [-1, 1, 100000],
+            "uint": [1, 1, 30000],
+            "float": [1.0, float("nan"), 1000],
+            "string": ["foo", "barbaz", ""],
+            "label": ["foo", "bar", "foo"],
             # "int": random_values(dtypes.int_dtype, num_rows),
             # "float": random_values(dtypes.float_dtype, num_rows),
             "embedding": [[1, 2, 3, 4], [1, 6, 3, 7], [-1, -2, -3, -4]],
-            "sequence_1d": [[1, 2, 3, 4], [1, 6, 3], [-1, -2, -3, -4, 10]],
+            # HF sequence as Spotlight sequence
+            "sequence_1d": [[1, 2, 3, 4], [1, 6, 3], [-1, -2, float("nan"), -4, 10]],
             "sequence_2d": [
                 [[1, 2, 3, 4], [-1, 3, 1, 6]],
                 [[1, -3, 10], [1, 6, 3]],
                 [[-10, 0, 10], [-1, -2, -3]],
             ],
             "sequence_2d_t": [[[5, 3], [2, 5], [10, 8]], [], [[-1, 1], [1, 10]]],
+            # HF sequence as Spotlight array
+            "sequence_2d_array": [
+                [[1, 2, 3, 4], [-1, 3, 1, 6], [1, 2, 4, 4]],
+                [[1, -3, 10], [1, 6, 3], [1, float("nan"), 4]],
+                [[-10, 0, 10], [-1, -2, -3], [1, 2, 4]],
+            ],
+            "sequence_3d_array": [
+                [[[1, 2, 3, 4], [-1, 3, 1, 6], [1, 2, 4, 4]]],
+                [[[1, -3, 10], [1, 6, 3], [1, float("nan"), 4]]],
+                [[[-10, 0, 10], [-1, -2, -3], [1, 2, 4]]],
+            ],
+            # HF 2D array as Spotlight sequence
+            "array_2d_sequence": [
+                [[1, 2, 3], [-1, 3, 1]],
+                [[1, -3, 10], [1, 6, 3]],
+                [[-10, 0, 10], [-1, -2, -3]],
+            ],
+            "array_2d_t_sequence": [
+                [[5, 3], [2, 5], [10, 8]],
+                [[float("nan"), 1], [1, 1], [2, 2]],
+                [[-1, 1], [1, 10], [10, 1]],
+            ],
+            "array_2d_vlen_sequence": [
+                [[5, 3], [2, 5], [10, 8]],
+                [],
+                [[-1, 1], [1, 10]],
+            ],
+            # HF 4D array as Spotlight array
+            "array_4d": [
+                [[[[1.0, 1.0, -10.0]]], [[[-1.0, 1.0, -1.0]]], [[[2.0, 1.0, 1.0]]]],
+                [
+                    [[[2.0, -3.0, 0.0]]],
+                    [[[3.0, 6.0, -2.0]]],
+                    [[[4.0, float("nan"), 2.0]]],
+                    [[[4.0, float("nan"), 2.0]]],
+                ],
+                [[[[3.0, 10.0, 10.0]]], [[[6.0, 3.0, -3.0]]], [[[4.0, 4.0, 4.0]]]],
+            ],
+            # HF list as Spotlight embedding
+            "list_sequence": [[1, 2, 3, 4], [1, 6, 3, 7], [-1, -2, -3, -4]],
         },
         features=datasets.Features(
             {
+                "bool": datasets.Value("bool"),
+                "int": datasets.Value("int32"),
+                "uint": datasets.Value("uint16"),
+                "float": datasets.Value("float64"),
+                "string": datasets.Value("string"),
+                "label": datasets.ClassLabel(
+                    num_classes=4, names=["foo", "bar", "baz", "barbaz"]
+                ),
                 "embedding": datasets.Sequence(
                     feature=datasets.Value("float64"), length=4
                 ),
@@ -71,15 +126,36 @@ def create_hf_dataset(num_rows: int) -> None:
                         feature=datasets.Value("float64"), length=2
                     ),
                 ),
+                "sequence_2d_array": datasets.Sequence(
+                    feature=datasets.Sequence(feature=datasets.Value("float64")),
+                    length=3,
+                ),
+                "sequence_3d_array": datasets.Sequence(
+                    feature=datasets.Sequence(
+                        feature=datasets.Sequence(feature=datasets.Value("float64")),
+                        length=3,
+                    ),
+                    length=1,
+                ),
+                "array_2d_sequence": datasets.Array2D(shape=(2, 3), dtype="float64"),
+                "array_2d_t_sequence": datasets.Array2D(shape=(3, 2), dtype="float64"),
+                "array_2d_vlen_sequence": datasets.Array2D(
+                    shape=(None, 2), dtype="float64"
+                ),
+                "array_4d": datasets.Array4D(shape=(None, 1, 1, 3), dtype="float64"),
+                "list_sequence": [datasets.Value("float64")],
             }
         ),
         # info=datasets.DatasetInfo(),
         # split=datasets.NamedSplit,
     )
-    ds.save_to_disk("build/datasets/hf")
+    ds.save_to_disk("./build/datasets/hf")
     print(ds.features)
 
 
 if __name__ == "__main__":
     np.random.seed(42)
     create_hf_dataset(100)
+
+    ds = datasets.load_from_disk("./build/datasets/hf")
+    spotlight.show(ds)
