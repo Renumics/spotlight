ff58f249944571b9e76d6cca57ec671e13d3b995
vishnukaushik <vishnukoushik23@gmail.com>
2023-10-24 19:30:42 +0530

    Added Rouge score

--- src/lenses/RougeScoreLens.tsx
+++ src/lenses/RougeScoreLens.tsx
@@ -1,31 +1,26 @@
-
-import { log } from "console";
 import { Lens } from "../types";
+import rouge from 'rouge';
 
 const calculateRougeScore=(values: unknown[])=>{
-    return values;
+        return rouge.n(values[0], values[1])
+    // return 0;
 }
 
-const RougeScoreLens: Lens = ({values, columns})=>{
+const RougeScoreLens: Lens = ({values})=>{
     const result = calculateRougeScore(values)
-    return (
-        <>
-            RougeScoreView: {result}
-        </>
-    )
+    return (<>Rouge score: {result}</>)
 }
 
 RougeScoreLens.key="RougeScoreView"
 RougeScoreLens.dataTypes=["str"]
-RougeScoreLens.defaultHeight = 100
+RougeScoreLens.defaultHeight = 48
 RougeScoreLens.minHeight=22
-RougeScoreLens.maxHeight=100
+RougeScoreLens.maxHeight=512
 RougeScoreLens.multi=true
 RougeScoreLens.displayName="ROUGE Score"
 RougeScoreLens.filterAllowedColumns=(allColumns, selectedColumns) => {
-    // if(selectedColumns.length===2)
-    //     return selectedColumns
-    // else return []
-    return []
+    if(selectedColumns.length===2)
+        return selectedColumns
+    else return allColumns.filter(({type}) => type.kind === 'str')
 };
 export default RougeScoreLens
