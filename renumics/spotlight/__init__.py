2c24abd3ca0daf9f3fdca86a631b49a9a0aaf620
Alexander Druz <alexandersdruz@gmail.com>
2023-10-27 14:44:15 +0200

    Add more examples to root module

--- renumics/spotlight/__init__.py
+++ renumics/spotlight/__init__.py
@@ -25,9 +25,57 @@ Serving a CSV file:
     >>> from renumics import spotlight
     >>> from renumics.spotlight import dtypes
     >>> df = pd.read_csv("https://renumics.com/data/mnist/mnist-tiny.csv")
-    >>> viewer = spotlight.show(df, dtype={"image": dtypes.image_dtype}, port=5000, no_browser=True, wait=False)
+    >>> viewer = spotlight.show(
+    ...     df,
+    ...     dtype={"image": dtypes.image_dtype},
+    ...     port=5000,
+    ...     no_browser=True,
+    ...     wait=False,
+    ... )
     Spotlight running on http://127.0.0.1:5000/
     >>> spotlight.close()
+
+Serving a Hugging Face dataset:
+    >>> import datasets
+    >>> from renumics import spotlight
+    >>> ds = datasets.load_dataset("mnist", split="test")
+    >>> viewer = spotlight.show(ds, port=5000, no_browser=True, wait=False)
+    Spotlight running on http://127.0.0.1:5000/
+    >>> spotlight.close()
+
+Serving a H5 dataset:
+    >>> import datasets
+    >>> from renumics import spotlight
+    >>> with spotlight.Dataset("docs/example.h5", "w") as dataset:
+    ...     dataset.append_int_column("int", range(4))
+    ...     dataset.append_string_column("str", "foo")
+    ...     dataset.append_datetime_column("dt", datetime(2017, 01, 01, 12))
+    ...     dataset.append_categorical_column("cat", ["foo", "bar"] * 2)
+    >>> viewer = spotlight.show(
+    ...     "docs/example.h5", port=5000, no_browser=True, wait=False
+    ... )
+    Spotlight running on http://127.0.0.1:5000/
+    >>> spotlight.close()
+
+Serving multiple datasets:
+    >>> import datasets
+    >>> import pandas as pd
+    >>> from renumics import spotlight
+    >>> from renumics.spotlight import dtypes
+    >>> df = pd.read_csv("https://renumics.com/data/mnist/mnist-tiny.csv")
+    >>> df_viewer = spotlight.show(df, port=5000, no_browser=True, wait=False)
+    Spotlight running on http://127.0.0.1:5000/
+    >>> ds = datasets.load_dataset("mnist", split="test")
+    >>> ds_viewer = spotlight.show(ds, port=5001, no_browser=True, wait=False)
+    Spotlight running on http://127.0.0.1:5001/
+    >>> spotlight.viewers()
+    [http://127.0.0.1:5000/, http://127.0.0.1:5001/]
+    >>> spotlight.close(5000)
+    >>> spotlight.viewers()
+    [http://127.0.0.1:5001/]
+    >>> spotlight.close()
+    >>> spotlight.viewers()
+    []
 """
 
 from .__version__ import __version__  # noqa: F401
