1b201cb4a95faef4f0a006d7ed233cb8734be17d
Dominik Haentsch <dominik.haentsch@gmail.com>
2023-11-10 09:18:22 +0100

    feat: export draggable badges in lib.ts

--- src/lib.ts
+++ src/lib.ts
@@ -27,6 +27,8 @@ export { default as Dot } from './components/ui/Dot';
 export { default as Menu } from './components/ui/Menu';
 export { default as Spinner } from './components/ui/Spinner';
 export { default as Tooltip } from './components/ui/Tooltip';
+export { default as ColumnBadge } from './components/ui/ColumnBadge';
+export { default as CellBadge } from './components/ui/CellBadge';
 
 export { default as ColumnSelector } from './components/ColumnSelector';
 
