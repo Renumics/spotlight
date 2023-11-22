115ee9a082a37a5bad58c8d542840244c7d57b2f
Tarek <tarek.wilkening@renumics.com>
2023-11-22 15:57:09 +0100

    improve coloring and responsiveness

--- src/lenses/BoundingBoxLens/index.tsx
+++ src/lenses/BoundingBoxLens/index.tsx
@@ -1,50 +1,75 @@
 import useResizeObserver from '@react-hook/resize-observer';
-import { useRef, useEffect } from 'react';
-import tw, { styled } from 'twin.macro';
+import { useRef, useEffect, useCallback } from 'react';
 import { Lens } from '../../types';
 import { CategoricalDataType, SequenceDataType } from '../../datatypes';
 import { ColorsState, useColors } from '../../stores/colors';
+import { Dataset, useDataset } from '../../stores/dataset';
 import * as d3 from 'd3';
-
-const StyledContainer = styled.div`
-    ${tw`relative inline-block h-full w-full`};
-    img {
-        ${tw`block h-full w-full`}
-    }
-    svg {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-    }
-`;
 
 const BoundingBoxLens: Lens = ({ urls, values, columns }) => {
     const container = useRef<HTMLDivElement>(null);
     const svgRef = useRef<SVGSVGElement>(null);
     const imgRef = useRef<HTMLImageElement>(null);
 
-    const bboxIndex = columns.findIndex(
+    // In case of single bounding box with label
+    const bboxColumnIndex = columns.findIndex((col) => col.type.kind === 'BoundingBox');
+    const categoryColumnIndex = columns.findIndex(
+        (col) => col.type.kind === 'Category'
+    );
+
+    // In case of multiple bounding boxes per image
+    const bboxesColumnIndex = columns.findIndex(
         (col) => col.type.kind === 'Sequence' && col.type.dtype.kind === 'BoundingBox'
     );
-    const categoryIndex = columns.findIndex(
+    const categoriesColumnIndex = columns.findIndex(
         (col) => col.type.kind === 'Sequence' && col.type.dtype.kind === 'Category'
     );
-    const imageIndex = columns.findIndex((col) => col.type.kind === 'Image');
+    const imageColumnIndex = columns.findIndex((col) => col.type.kind === 'Image');
+    const url = urls[imageColumnIndex];
 
-    const boxes = values[bboxIndex] as [[number, number, number, number]] | undefined;
-    const url = urls[imageIndex];
-    const categories = values[categoryIndex] as [number] | undefined;
-    const invertedCategories = (
-        (columns[categoryIndex].type as SequenceDataType).dtype as CategoricalDataType
-    ).invertedCategories;
+    let boxes: [number[]] | [] = [];
+    let categories: number[] | [] = [];
+    let invertedCategories = (index: number) => index.toString();
+
+    // Check if single bounding box or multiple
+    if (bboxColumnIndex != -1) {
+        boxes = [values[bboxColumnIndex] as number[]];
+    } else if (bboxesColumnIndex != -1) {
+        boxes = values[bboxesColumnIndex] as [number[]];
+    }
+
+    if (categoryColumnIndex != -1) {
+        categories = [values[categoryColumnIndex] as number];
+
+        invertedCategories = (index) =>
+            (columns[categoryColumnIndex].type as CategoricalDataType)
+                .invertedCategories[index];
+    } else if (categoriesColumnIndex != -1) {
+        categories = values[categoriesColumnIndex] as [number];
+
+        invertedCategories = (index) =>
+            (
+                (columns[categoriesColumnIndex].type as SequenceDataType)
+                    .dtype as CategoricalDataType
+            ).invertedCategories[index];
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const colorTransferFunctionSelector = useCallback(
+        (d: Dataset) => {
+            if (categoryColumnIndex != -1) {
+                return d.colorTransferFunctions[columns[categoryColumnIndex].key]?.[
+                    'full'
+                ];
+            }
+            return undefined;
+        },
+        [columns, categoryColumnIndex]
+    );
 
     const colorPaletteSelector = (c: ColorsState) => c.continuousPalette;
     const colorPalette = useColors(colorPaletteSelector);
-
-    useResizeObserver(container.current, () => drawBoundingBoxes());
-    useEffect(() => drawBoundingBoxes());
+    const colorTransferFunction = useDataset(colorTransferFunctionSelector);
 
     const drawBoundingBoxes = () => {
         if (!svgRef.current) return;
@@ -56,18 +81,51 @@ const BoundingBoxLens: Lens = ({ urls, values, columns }) => {
         d3.select(svgRef.current).select<SVGSVGElement>('g').selectAll('rect').remove();
         d3.select(svgRef.current).select<SVGSVGElement>('g').selectAll('text').remove();
 
-        const colorScale = colorPalette.scale().colors(boxes.length);
-        const w: number = container.current.clientWidth;
-        const h: number = container.current.clientHeight;
+        let colorFunc: (i: number) => string;
+
+        if (categories.length === 0) {
+            colorFunc = (i: number) => colorPalette.scale().colors(boxes.length)[i];
+        } else {
+            if (colorTransferFunction !== undefined) {
+                colorFunc = (i: number) => colorTransferFunction(categories[i]).hex();
+            } else {
+                colorFunc = (i: number) =>
+                    colorPalette.scale().colors(categories.length)[i];
+            }
+        }
+
+        const image = imgRef.current;
+        // Natural dimensions of the image
+        const naturalWidth = image.naturalWidth;
+        const naturalHeight = image.naturalHeight;
+        const imageAspectRatio = naturalWidth / naturalHeight;
+
+        // Dimensions of the parent element
+        const parentWidth = container.current.offsetWidth;
+        const parentHeight = container.current.offsetHeight;
+        const parentAspectRatio = parentWidth / parentHeight; // Natural dimensions of the image
+
+        let renderedWidth, renderedHeight;
+
+        if (imageAspectRatio > parentAspectRatio) {
+            renderedWidth = parentWidth;
+            renderedHeight = parentWidth / imageAspectRatio;
+        } else {
+            renderedHeight = parentHeight;
+            renderedWidth = parentHeight * imageAspectRatio;
+        }
+        const offsetWidth = (parentWidth - renderedWidth) / 2;
+        const offsetHeight = (parentHeight - renderedHeight) / 2;
 
         for (let i = 0; i < boxes.length; i++) {
             const box = boxes[i];
 
             // Box format [x, y, w, h] normalized.
-            const x = box[0] * w;
-            const y = box[1] * h;
-            const width = (box[2] - box[0]) * w;
-            const height = (box[3] - box[1]) * h;
+            const x = box[0] * renderedWidth + offsetWidth;
+            const y = box[1] * renderedHeight + offsetHeight;
+            const width = (box[2] - box[0]) * renderedWidth;
+            const height = (box[3] - box[1]) * renderedHeight;
+            const boxColor = colorFunc(i);
 
             d3.select(svgRef.current)
                 .select<SVGSVGElement>('g')
@@ -78,38 +136,96 @@ const BoundingBoxLens: Lens = ({ urls, values, columns }) => {
                 .attr('height', height)
                 .attr('stroke', 'firebrick')
                 .attr('stroke-width', 2)
-                .attr('stroke', colorScale[i])
+                .attr('stroke', boxColor)
                 .attr('fill', 'none');
 
-            if (categories) {
+            if (categories.length > 0) {
                 d3.select(svgRef.current)
                     .select<SVGSVGElement>('g')
                     .append('text')
-                    .text(invertedCategories[categories[i]])
+                    .text(invertedCategories(categories[i]))
                     .attr('x', x)
                     .attr('y', y - 3)
                     .attr('fontsize', 12)
-                    .attr('fill', 'red');
+                    .attr('fill', boxColor);
             }
         }
     };
 
+    useResizeObserver(container.current, () => drawBoundingBoxes());
+    useEffect(() => drawBoundingBoxes());
+
     return (
-        <div tw="h-full w-full">
-            <StyledContainer ref={container}>
-                <img ref={imgRef} src={url} alt="Not found." />
-                <svg ref={svgRef}>
+        <div tw="relative w-full h-full max-w-full block">
+            <div ref={container} tw="relative h-full w-full overflow-hidden">
+                <img
+                    ref={imgRef}
+                    src={url}
+                    alt="URL not found!"
+                    tw="absolute h-full w-full top-0 left-0 object-contain"
+                />
+                <svg ref={svgRef} tw="absolute top-0 left-0 w-full h-full">
                     <g />
                 </svg>
-            </StyledContainer>
+            </div>
         </div>
     );
 };
 
 BoundingBoxLens.key = 'BoundingBoxView';
-BoundingBoxLens.dataTypes = ['Image', 'BoundingBox', 'Sequence'];
+BoundingBoxLens.dataTypes = ['Image', 'BoundingBox', 'Sequence', 'Category'];
 BoundingBoxLens.multi = true;
 BoundingBoxLens.displayName = 'BoundingBox';
 BoundingBoxLens.defaultHeight = 256;
+BoundingBoxLens.filterAllowedColumns = (allColumns, selectedColumns) => {
+    if (selectedColumns.length === 3) return [];
+
+    const selectedTypes = selectedColumns.map((selectedCol) => selectedCol.type);
+    if (selectedColumns.length === 0)
+        return allColumns.filter(({ type }) =>
+            BoundingBoxLens.dataTypes.includes(type.kind)
+        );
+
+    if (selectedTypes.find((type) => type.kind === 'Sequence')) {
+        return allColumns.filter(({ type }) => {
+            return type.kind === 'Sequence' || type.kind === 'Image';
+        });
+    } else if (
+        selectedTypes.find((type) => ['BoundingBox', 'Category'].includes(type.kind))
+    ) {
+        return allColumns.filter(({ type }) => {
+            return (
+                type.kind === 'BoundingBox' ||
+                type.kind === 'Category' ||
+                type.kind === 'Image'
+            );
+        });
+    } else {
+        return allColumns.filter(({ type }) =>
+            BoundingBoxLens.dataTypes.includes(type.kind)
+        );
+    }
+};
+BoundingBoxLens.isSatisfied = (columns) => {
+    if (columns.length < 2) return false;
+
+    const types = columns.map((col) => col.type);
+
+    if (!types.find((type) => type.kind === 'Image')) return false;
+
+    if (!types.find((type) => ['BoundingBox', 'Sequence'].includes(type.kind)))
+        return false;
+
+    if (
+        types.find(
+            (type) => type.kind === 'Sequence' && type.dtype.kind === 'BoundingBox'
+        )
+    ) {
+        return true;
+    } else {
+        if (types.find((type) => type.kind === 'BoundingBox')) return true;
+        else return false;
+    }
+};
 
 export default BoundingBoxLens;
